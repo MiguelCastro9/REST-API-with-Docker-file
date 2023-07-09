127108d78559c8b73a4b85f771d1cf953b6f4f58
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-07-09 10:46:47 -0300

    ajustes e documentando api

--- src/main/java/com/api/controller/ProdutoController.java
+++ src/main/java/com/api/controller/ProdutoController.java
@@ -1,9 +1,12 @@
 package com.api.controller;
 
 import com.api.dto.ProdutoDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/produto")
+@Tag(name = "Produto controller")
 public class ProdutoController {
 
     @PostMapping
-    public ResponseEntity<ProdutoDto> enviar() {
+    @Operation(description = "Enviar produto")
+    public ResponseEntity<ProdutoDto> enviar(@RequestBody ProdutoDto produtoDto) {
+        System.out.println(produtoDto);
         return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/api/dto/ProdutoDto.java
+++ src/main/java/com/api/dto/ProdutoDto.java
@@ -1,9 +1,12 @@
 package com.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  *
  * @author Miguel Castro
  */
+@Schema(name = "Produto")
 public class ProdutoDto {
     
     private Long codigo_produto;
@@ -13,4 +16,41 @@ public class ProdutoDto {
     private Double valor_produto;
     
     private Integer quantidade_produto;
+
+    public Long getCodigo_produto() {
+        return codigo_produto;
+    }
+
+    public void setCodigo_produto(Long codigo_produto) {
+        this.codigo_produto = codigo_produto;
+    }
+
+    public String getNome_produto() {
+        return nome_produto;
+    }
+
+    public void setNome_produto(String nome_produto) {
+        this.nome_produto = nome_produto;
+    }
+
+    public Double getValor_produto() {
+        return valor_produto;
+    }
+
+    public void setValor_produto(Double valor_produto) {
+        this.valor_produto = valor_produto;
+    }
+
+    public Integer getQuantidade_produto() {
+        return quantidade_produto;
+    }
+
+    public void setQuantidade_produto(Integer quantidade_produto) {
+        this.quantidade_produto = quantidade_produto;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdutoDto{" + "codigo_produto=" + codigo_produto + ", nome_produto=" + nome_produto + ", valor_produto=" + valor_produto + ", quantidade_produto=" + quantidade_produto + '}';
+    }
 }
